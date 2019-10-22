d11cf14a2d57295d9d246ba4df6edcf7059792ca
Irfan Indra Kurniawan <50232836+irVeanKun@users.noreply.github.com>
2019-10-22 18:32:27 +0700

    Revisi Daftar Komponen

--- Dump_Data_Remote/Dump_Data_Remote.ino
+++ Dump_Data_Remote/Dump_Data_Remote.ino
@@ -4,13 +4,16 @@ int RECV_PIN = 11; // PIN 11
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   irrecv.enableIRIn();
 }
-void loop() {
-  if (irrecv.decode(&results)) {
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
     Serial.println(results.value);
-    dump(&results);
-    irrecv.resume(); // 
+    irrecv.resume();
+  }
 }
